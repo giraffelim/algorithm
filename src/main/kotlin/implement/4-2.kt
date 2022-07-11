2e3d75644b93e6ce8b9cf0de25c03c3fd11f7977
giraffelim <taeyang7704@naver.com>
2022-07-11 22:44:01 +0900

    implement: 시각

---
+++ src/main/kotlin/implement/4-2.kt
@@ -0,0 +1,25 @@
+package implement
+
+import java.util.Scanner
+
+fun solutionByMe(n: Int) {
+    var result = 0
+
+    for(t in 0..n) {
+        for (m in 0 .. 59) {
+            for (s in 0 .. 59) {
+                if ("${t}${m}${s}".contains('3')) {
+                    result++
+                }
+            }
+        }
+    }
+
+    println(result)
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    solutionByMe(n)
+}

cae823e0a95090c526517e8c92732e3844bd1733
giraffelim <taeyang7704@naver.com>
2022-07-24 01:03:04 +0900

    sort: 두 배열의 원소 교체

---
+++ src/main/kotlin/sort/6-12.kt
@@ -0,0 +1,31 @@
+package sort
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val k = sc.nextInt()
+
+    val arrA = mutableListOf<Int>()
+    val arrB = mutableListOf<Int>()
+
+    for (i in 0 until n) {
+        arrA.add(sc.nextInt())
+    }
+
+    for (i in 0 until n) {
+        arrB.add(sc.nextInt())
+    }
+
+    arrA.sort()
+    arrB.sortDescending()
+
+    for (i in 0 until k) {
+        val temp = arrA[i]
+        arrA[i] = arrB[i]
+        arrB[i] = temp
+    }
+
+    println(arrA.sum())
+}
